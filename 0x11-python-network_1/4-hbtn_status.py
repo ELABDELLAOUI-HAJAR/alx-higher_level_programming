c1929dc63a3cc7aedb7e654a2d23b2872bf5bbd2
HAJAR <elabdellaoui.hajar@gmail.com>
2023-09-29 20:16:06 +0000

    Python script that fetches https://alx-intranet.hbtn.io/status by using requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches
+https://alx-intranet.hbtn.io/status
+by using requests package
+"""
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
