491e4c2134325061dd4fcad71f3920e66a01c577
HAJAR <elabdellaoui.hajar@gmail.com>
2023-09-29 22:48:24 +0000

    advancef

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""List 10 commits (from the most recent to oldest)
+    of a repository of a user
+    * Python script that takes 2 arguments:
+        * The first argument will be the repository name
+        * The second argument will be the owner name
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits"
+    resp = requests.get(url.format(argv[2], argv[1]))
+    res = resp.json()
+    try:
+        for j in range(0, 10):
+            print(f"{res[j].get('sha')}: \
+{res[j].get('commit').get('author').get('name')}")
+    except IndexError:
+        pass
