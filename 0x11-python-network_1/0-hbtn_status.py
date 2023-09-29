9d9c27eb76964b94fe43c9916ec8d6e264aa0198
HAJAR <elabdellaoui.hajar@gmail.com>
2023-09-29 16:43:55 +0000

    fix task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-# script that fetches https://alx-intranet.hbtn.io/status
+"""script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        body = res.read()
+    request = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(request) as response:
+        html_body = response.read()
         print("Body response:")
-        print("    - type: {}".format(type(body)))
-        print("    - content: {}".format(body))
-        print("    - utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(html_body)))
+        print("\t- content: {}".format(html_body))
+        print("\t- utf8 content: {}".format(html_body.decode("utf-8")))
