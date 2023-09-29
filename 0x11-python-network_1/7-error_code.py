dc275503d2191a75a16f7f7579cae001f34dad01
HAJAR <elabdellaoui.hajar@gmail.com>
2023-09-29 21:07:56 +0000

    Python script that takes in a URL, sends a request to the URL and displays the body of the response by using requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to
+the URL and displays the body of the response by using requests
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print(f"Error code: {response.status_code}")
